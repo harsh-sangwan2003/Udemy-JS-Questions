015022a2dad6769de57ae169bbcc48d0ec0c76a9
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-10-07 09:06:47 +0530

    added

--- 01_Core JS/12_shuffleFunction.js
+++ 01_Core JS/12_shuffleFunction.js
@@ -1,1 +1,9 @@
 // Write a Function which implement shuffle
+const shuffleItems = (items) => {
+
+    return items.map(item => ({ sort: Math.random(), value: item }))
+        .sort((item1, item2) => item1.sort - item2.sort)
+        .map(a => a.value);
+}
+
+console.log(shuffleItems([1,2]));
